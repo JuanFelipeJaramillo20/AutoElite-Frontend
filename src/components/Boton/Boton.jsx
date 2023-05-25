9d842c5a7cf39f46985b377294ebc14948c65c21
Paoladevelopment <paolala1017@gmail.com>
2023-05-25 18:36:47 -0500

    PFG1DS1-85 => Se sube el header con lo necesitado hasta el momento

--- src/components/Boton/Boton.jsx
+++ src/components/Boton/Boton.jsx
@@ -1,18 +1,17 @@
 import PropTypes from 'prop-types';
 import './Boton.css';
 export const Boton = (props) => {
-  const { imageSrc, descripcionImg, texto, tipo, onClick } = props;
+  const { classIcon, texto, tipo, onClick } = props;
   return (
     <button className='app-btn' type={tipo} onClick={onClick}>
+      {classIcon && <i className={classIcon}> </i>}
       {texto}
-      {imageSrc && <img src={imageSrc} alt={descripcionImg}></img>}
     </button>
   );
 };
 
 Boton.propTypes = {
-  imageSrc: PropTypes.string,
-  descripcionImg: PropTypes.string,
+  classIcon: PropTypes.string,
   texto: PropTypes.string,
   tipo: PropTypes.string,
   onClick: PropTypes.func,
