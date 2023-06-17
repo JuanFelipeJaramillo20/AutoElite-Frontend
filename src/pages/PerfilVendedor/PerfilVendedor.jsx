a11b6a478d3785352fb552bc73006bbcc0312dd0
Paoladevelopment <paolala1017@gmail.com>
2023-06-16 22:36:08 -0500

    PFG1DS1-124 => El perfil del vendedor está más o menos dinamico, faltan las reviews para que quede completo. Se cambio la foto de perfil predeterminada de los usuarios también

--- src/pages/PerfilVendedor/PerfilVendedor.jsx
+++ src/pages/PerfilVendedor/PerfilVendedor.jsx
@@ -1,149 +1,164 @@
-import { useRef, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import { AddCalificacion } from './components/AddCalificacion/AddCalificacion';
 import { CardCar } from '../../components/CardCar/CardCar';
 import { Form } from '../../components/Form/Form';
 
-import { USUARIOS, PUBLICACION, CARROS, REVIEWS } from '../../../constants';
-import carrosJuanda from '../../assets/img/inicio/elMejor.jpeg';
-import fotoProfile from '../../assets/img/perfil/perfil-ejemplo.jpg';
+import { REVIEWS } from '../../../constants';
+import fotoProfile from '../../assets/img/perfil/usuario.png';
 
 import './PerfilVendedor.css';
+import { useParams } from 'react-router';
 
 export const PerfilVendedor = () => {
-  const vendorProfile = useRef();
-  const vendorMore = useRef();
+  const { usuarioId } = useParams();
+  const [usuario, setUsuario] = useState(null);
+  const [publicaciones, setPublicaciones] = useState(null);
 
+  const obtenerPublicaciones = async (emailAutor) => {
+    const response = await fetch(
+      `http://localhost:8080/api/v1/publicaciones/byuser/${emailAutor}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+
+    const result = await response.json();
+    if (response.ok) {
+      setPublicaciones(result);
+    }
+  };
   useEffect(() => {
-    const handleResize = () => {
-      const areDefined = vendorProfile.current && vendorMore.current;
-      const iswider = window.innerWidth > 925;
-      if (areDefined) {
-        if (iswider) {
-          let moreHeight = vendorMore.current.clientHeight;
-          vendorProfile.current.style.height = `${moreHeight}px`;
-        } else {
-          vendorProfile.current.style.height = `auto`;
+    const getDatosUsuario = async (idUsuario) => {
+      const response = await fetch(
+        `http://localhost:8080/api/v1/usuarios/${idUsuario}`,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
         }
+      );
+
+      const result = await response.json();
+      if (response.ok) {
+        setUsuario(result);
       }
     };
-    handleResize();
-    window.addEventListener('resize', handleResize);
 
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
+    getDatosUsuario(usuarioId);
   }, []);
 
-  return (
-    <>
-      <section className='vendor-section'>
-        <header ref={vendorProfile} className='vendor-section__profile'>
-          <div className='profile-data'>
-            <div className='profile-data__img'>
-              <img src={fotoProfile} alt="user picture" />
-            </div>
-            <div className='profile-data__personal-data'>
-              <h2>
-                {USUARIOS[0].Nombre}
-              </h2>
-              <p>
-                {USUARIOS[0].Teléfono}
-              </p>
-            </div>
+  useEffect(() => {
+    if (usuario !== null) {
+      obtenerPublicaciones(usuario.email);
+    }
+  }, [usuario]);
+
+  return usuario !== null ? (
+    <section className='vendor-section'>
+      <header className='vendor-section__profile'>
+        <div className='profile-data'>
+          <div className='profile-data__img'>
+            <img src={fotoProfile} alt='user picture' />
           </div>
-          <div className='profile-data_about'>
-            <h2>
-              Carros disponibles {3}
-            </h2>
-            <h2>
-              Carros vendidos {10}
-            </h2>
+          <div className='profile-data__personal-data'>
+            <h2>{usuario.nombres}</h2>
+            <p>{usuario.telefono}</p>
           </div>
-          <div className='about-form'>
-            <Form
-              inputs={[
-                {
-                  type: 'text',
-                  id: 'name-send',
-                  label: 'Nombre completo',
-                  placeHolder: 'Digita tu nombre',
-                  validacion: {
-                    required: true,
-                  },
-                  error: {
-                    required: 'El nombre es obligatorio.',
-                  },
+        </div>
+        <div className='profile-data_about'>
+          <h2>{usuario.email}</h2>
+          {publicaciones !== null ? (
+            <h2>Carros publicados {publicaciones.length}</h2>
+          ) : (
+            <h2>Carros publicados 0</h2>
+          )}
+        </div>
+        <div className='about-form'>
+          <Form
+            inputs={[
+              {
+                type: 'text',
+                id: 'name-send',
+                label: 'Nombre completo',
+                placeHolder: 'Digita tu nombre',
+                validacion: {
+                  required: true,
                 },
-                {
-                  type: 'email',
-                  id: 'email-send',
-                  label: 'Correo electrónico',
-                  placeHolder: 'Digita tu correo',
-                  validacion: {
-                    required: true,
-                    pattern: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
-                  },
-                  error: {
-                    required: 'El nombre es obligatorio.',
-                    pattern: 'Correo electrónico no válido',
-                  },
+                error: {
+                  required: 'El nombre es obligatorio.',
                 },
-                {
-                  type: 'textarea',
-                  id: 'message-send',
-                  label: 'Motivo',
-                  placeHolder: 'Quiero comprar tu carro!',
-                  validacion: {
-                    required: true,
-                  },
-                  error: {
-                    required: 'El motivo es obligatorio.',
-                  },
+              },
+              {
+                type: 'email',
+                id: 'email-send',
+                label: 'Correo electrónico',
+                placeHolder: 'Digita tu correo',
+                validacion: {
+                  required: true,
+                  pattern: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
                 },
-              ]}
-              btnText={'Enviar mensaje'}
-              onSubmit={(data) => {
-                console.log(data);
-              }}
-            />
-          </div>
-        </header>
-        <article ref={vendorMore} className='vendor-section__more'>
-          <div className='more-title'>
-            <h2>
-              Carros disponibles
-            </h2>
-          </div>
-          <div className='more__cars'>
-            {PUBLICACION.map((el, id) => {
-              if (id < 3) {
-                return (
-                  <div key={el.IDCarro}>
-                    <CardCar
-                      ciudadVenta={el.Ciudad}
-                      idPublicacion={el.IDPublicación}
-                      kilometraje={CARROS[id].Kilómetros}
-                      marcaCarro={CARROS[id].Marca}
-                      modeloCarro={CARROS[id].Modelo}
-                      precio={CARROS[id].Precio}
-                      tipoCombustible={CARROS[id].Combustible}
-                      tipoTransmision={CARROS[id].Transmisión}
-                      yearCarro={CARROS[id].Año}
-                      srcImageCar={carrosJuanda}
-                    />
-                  </ div>
-                );
-              }
-            })}
-          </div>
-        </article>
-        <div className='vendor-section__reviews'>
-          <AddCalificacion
-            totalReviewsVendor={REVIEWS}
+                error: {
+                  required: 'El nombre es obligatorio.',
+                  pattern: 'Correo electrónico no válido',
+                },
+              },
+              {
+                type: 'textarea',
+                id: 'message-send',
+                label: 'Motivo',
+                placeHolder: 'Quiero comprar tu carro!',
+                validacion: {
+                  required: true,
+                },
+                error: {
+                  required: 'El motivo es obligatorio.',
+                },
+              },
+            ]}
+            btnText={'Enviar mensaje'}
+            onSubmit={(data) => {
+              console.log(data);
+            }}
           />
         </div>
-      </section>
-    </>
-  );
+      </header>
+      <article className='vendor-section__more'>
+        <div className='more-title'>
+          <h2>Carros disponibles</h2>
+        </div>
+        <div className='more__cars'>
+          {publicaciones !== null ? (
+            publicaciones.map((publicacion) => {
+              return (
+                <CardCar
+                  key={publicacion.id}
+                  idPublicacion={publicacion.id}
+                  srcImageCar='https://i.imgur.com/xyiSDoE.jpeg'
+                  yearCarro={publicacion.carroPublicacion.year}
+                  modeloCarro={publicacion.carroPublicacion.tipo}
+                  marcaCarro={publicacion.carroPublicacion.marca}
+                  precio={publicacion.carroPublicacion.precio}
+                  ciudadVenta={publicacion.carroPublicacion.ciudad}
+                  kilometraje={publicacion.carroPublicacion.kilometraje}
+                  tipoTransmision={publicacion.carroPublicacion.transmision}
+                  tipoCombustible={publicacion.carroPublicacion.combustible}
+                  estado={publicacion.carroPublicacion.estado}
+                ></CardCar>
+              );
+            })
+          ) : (
+            <h2>Ninguno</h2>
+          )}
+        </div>
+      </article>
+      <div className='vendor-section__reviews'>
+        <AddCalificacion totalReviewsVendor={REVIEWS} />
+      </div>
+    </section>
+  ) : null;
 };
